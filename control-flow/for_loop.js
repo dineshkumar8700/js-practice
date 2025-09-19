37aea726aa97d971a61767bcad2ec5c67ecf9ee9
Dinesh Kumar <mk9211620@gmail.com>
2025-09-19 14:22:32 +0530

    Create for_loop.js for finding factorial of a number.

---
+++ control-flow/for_loop.js
@@ -0,0 +1,10 @@
+// Finding Factorial of a number using For Loop 
+
+const inputNumber = 5;
+let currentTerm = 1;
+let product = 1;
+
+for(let i = 1; i<=inputNumber; i++){
+    product = product * i;
+}
+console.log(product);
