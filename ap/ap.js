409981ce9ea8ee6c8ea0bf75e9d609c17b8d268e
Dinesh Kumar <mk9211620@gmail.com>
2025-09-19 17:50:54 +0530

    Create ap.js that prints AP from a number to a certain number.

---
+++ ap/ap.js
@@ -0,0 +1,15 @@
+// Finding AP from a number to a certain number 
+
+const firstTerm = 2;
+const difference = 4;
+let limit = 7;
+let numberOfTerms = 1;
+let currentTerm = firstTerm;
+
+console.log("First Term:", firstTerm, "Difference:", difference, "Limit:", limit)
+console.log(firstTerm);
+while(numberOfTerms < limit){
+    currentTerm += difference;
+    console.log(currentTerm);    
+    numberOfTerms++;
+}
