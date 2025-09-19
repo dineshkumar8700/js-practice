99825ca7c1e71991f484fc6f94dc697a29e71908
Dinesh Kumar <mk9211620@gmail.com>
2025-09-19 14:08:02 +0530

    Create while_loop.js to print sum of n numbers

---
+++ control-flow/while_loop.js
@@ -0,0 +1,10 @@
+// Printing sum of n terms using while loop
+let currentTerm = 1
+let finalTerm = 10
+let sum = 0
+
+while(currentTerm <=finalTerm){
+    sum += currentTerm;
+    console.log("Sum", sum, "Current Term", currentTerm);
+    currentTerm +=1;
+}
