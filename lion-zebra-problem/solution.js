283fe15b45183913e29df659f23ae8311aa42cf1
Dinesh Kumar <mk9211620@gmail.com>
2025-09-20 22:13:55 +0530

    Added feature that checks whether the provided input is valid or not.

--- lion-zebra-problem/solution.js
+++ lion-zebra-problem/solution.js
@@ -1,12 +1,22 @@
 const testCase1 = "LZ";
 const testCase2 = "L Z";
 const testCase3 = "L ZL";
+let validTestCase = false;
 let shortestSpace;
 let currentSpace;
 
 const testCaseToUse = testCase1;
 
-// Write your code here:
+// Checking whether the provided input is valid or not.
+for(let i = 0; i<testCaseToUse.length; i++){
+    if(testCaseToUse[i] === "Z" || testCaseToUse[i] == "L" ){
+        validTestCase = true;    
+    }
+}   
 
-
-console.log("Input:", testCaseToUse, "Output:", /* your result here */);
+if(validTestCase){
+    console.log("Input:", testCaseToUse, "Output:", "0");
+}
+else{
+    console.log("-1 \nInvalied Input. Kindly enter Z & L in the input string.")
+}
