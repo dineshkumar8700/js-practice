eb6e072f8eb7989fe8df2f291b2cdfe034f6b4aa
Dinesh Kumar <mk9211620@gmail.com>
2025-09-20 21:49:00 +0530

    Create solution.js and set up file template

---
+++ lion-zebra-problem/solution.js
@@ -0,0 +1,11 @@
+const testCase1 = "LZ";
+const testCase2 = "L Z";
+const testCase3 = "L ZL";
+
+const testCaseToUse = testCase1;
+
+
+// Write your code here:
+
+
+console.log("Input:", testCaseToUse, "Output:", /* your result here */);
