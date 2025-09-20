9a59b2ce23dc37606b6a5db7c01c76274ccbaffd
Dinesh Kumar <mk9211620@gmail.com>
2025-09-20 21:54:03 +0530

    Added shortestSpace & currentSpace variable to keep track of spaces between lion & Zebra.

--- lion-zebra-problem/solution.js
+++ lion-zebra-problem/solution.js
@@ -1,9 +1,10 @@
 const testCase1 = "LZ";
 const testCase2 = "L Z";
 const testCase3 = "L ZL";
+let shortestSpace;
+let currentSpace;
 
 const testCaseToUse = testCase1;
-
 
 // Write your code here:
 
