945b2e04f97cc4b42ba7700b61fac3fa555a425b
Dinesh Kumar <mk9211620@gmail.com>
2025-09-21 14:00:03 +0530

    Competed program for handling all input to give expected output.

--- lion-zebra-problem/solution.js
+++ lion-zebra-problem/solution.js
@@ -1,11 +1,14 @@
-const testCase1 = "LZ";
-const testCase2 = "L Z";
-const testCase3 = "L ZL";
-let validTestCase = false;
-let shortestSpace;
-let currentSpace;
+const testCase1 = "L     L Z";
+const testCase2 = "L   Z";
+const testCase3 = "L   Z  L";
+const testCase4 = "L  ZZ L Z";
+const testCase5 = "L  Z   Z  L Z";
+const testCase6 = "L  Z   Z L L   L";
+const testCase7 = "L  Z L  Z";
 
-const testCaseToUse = testCase1;
+let validTestCase = false;
+
+const testCaseToUse = testCase2;
 
 // Checking whether the provided input is valid or not.
 for(let i = 0; i<testCaseToUse.length; i++){
@@ -14,9 +17,51 @@ for(let i = 0; i<testCaseToUse.length; i++){
     }
 }   
 
+// Finding shortest number of spaces between any lion and any zebra
+let shortestSpace = Infinity;
+let currentSpace = 0;
+let currentShortestSpace = 0;
+
+for(let i = 0; i < testCaseToUse.length; i++){
+    if(testCaseToUse[i] === "L"){
+        for(let j = 1; j <testCaseToUse.length; j++){
+            if(testCaseToUse[i+j] === "Z"){
+                currentShortestSpace = currentSpace;
+            }
+            else if(testCaseToUse[i+j] === "L"){
+                currentSpace = 0;
+            }
+            else{
+                currentSpace += 1;
+            }
+        }
+        shortestSpace = (currentShortestSpace < shortestSpace)? currentShortestSpace:shortestSpace
+        currentSpace = 0;
+    }
+    else if(testCaseToUse[i] === "Z"){
+        for(let j = 1; j < testCaseToUse.length; j++){
+            if(testCaseToUse[i+j] === "L"){
+
+                currentShortestSpace = currentSpace;
+                shortestSpace = (currentShortestSpace < shortestSpace)? currentShortestSpace:shortestSpace;
+
+            }
+            else if(testCaseToUse[i+j] === "Z"){
+                currentSpace = 0;
+            }
+            else{
+                currentSpace += 1;
+            }
+        }
+        shortestSpace = (currentShortestSpace < shortestSpace)? currentShortestSpace:shortestSpace;
+    }
+}      
+
+shortestSpace = (currentShortestSpace < shortestSpace)? currentShortestSpace:shortestSpace;
+
 if(validTestCase){
-    console.log("Input:", testCaseToUse, "Output:", "0");
+    console.log("Input:", testCaseToUse, "Output:",shortestSpace);
 }
 else{
-    console.log("-1 \nInvalied Input. Kindly enter Z & L in the input string.")
+    console.log("-1 \nInvalied Input. Kindly enter Z & L in the input string.");
 }
