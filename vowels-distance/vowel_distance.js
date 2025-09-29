4ad9be4078738c9361784b35f150644174842fb1
Dinesh Kumar <mk9211620@gmail.com>
2025-09-29 11:41:30 +0530

    Improved taste of the program.

--- vowels-distance/vowel_distance.js
+++ vowels-distance/vowel_distance.js
@@ -2,10 +2,22 @@ function findDistancebetweenVowel(string) {
   return 3;
 }
 
+function composeMessage(mark, inputSection, actualSection, expectedSection) {
+  let message = mark + " | ";
+  message += inputSection + " | ";
+  message +=  actualSection + " ";
+  message += expectedSection;
+  console.log(message);
+}
+
 function testIsVowel(string, expected) {
   const actual = findDistancebetweenVowel(string);
   const mark = actual === expected ? "✅" : "❌";
-  console.log(mark, actual, expected);
+  const inputSection = string;
+  const actualSection = "actual:" + " " + actual;
+  const expectedSection = "expected:" + " " + expected;
+
+  composeMessage(mark, inputSection, actualSection, expectedSection);
 }
 
 
