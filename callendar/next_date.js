b26cab7e3c858ee1f4c8de06f6858aada732f6cf
Dinesh Kumar <mk9211620@gmail.com>
2025-10-06 12:24:13 +0530

    added compose message for failing test

--- callendar/next_date.js
+++ callendar/next_date.js
@@ -1,22 +1,38 @@
-function incrementDate(date, month, year) {
+function incrementDate(date) {
   return "02-01-2025";
 }
 
-function composeMessage(mark, message) {
-  console.log(mark, message);
+function composeFailCase(msg, inputSection, actualSection, expectedSection) {
+  let message = "❌ " + msg + "\n";
+  message += inputSection + "\n";
+  message += actualSection + "\n";
+  message += expectedSection;
+
+  console.log(message);
 }
 
-function testIncrementDate(message, date, month, year, expected) {
-  const actual = incrementDate(date, month, year);
-  const mark = actual === expected ? '✅' : '❌';
+function composeMessage(testType) {
+  const message = "✅ " + testType;
+
+  console.log(message);
+}
+
+function testIncrementDate(message, date, expected) {
+  const actual = incrementDate(date);
 
   if (actual === expected) {
-    composeMessage(mark, message);
+    composeMessage(message);
+  } else {
+    const inputSectioin = "Input    : " + date;
+    const actualSection = "actual   :" + " " + actual;
+    const expectedSection = "expected :" + " " + expected;
+    composeFailCase(message, inputSectioin, actualSection, expectedSection);
   }
+
 }
 
 function testAll() {
-  testIncrementDate("First day of year", 1, 1, 2025, "02-01-2025");
+  testIncrementDate("First day of year", "01-02-2025", "02-01-2025");
 }
 
 function main() {
