18b120d90a3b330cee0dc874bd1d9a137fd7889a
Dinesh Kumar <mk9211620@gmail.com>
2025-10-06 15:16:23 +0530

    Update next_date.js

--- callendar/next_date.js
+++ callendar/next_date.js
@@ -21,18 +21,38 @@ function isLeapYear(year) {
 }
 
 function isLastDayOfMonth(month, year) {
-  switch(month) {
+  switch (month) {
     case 2: return isLeapYear(year) ? 29 : 28;
-    case 4: 
-    case 6: 
-    case 9: 
+    case 4:
+    case 6:
+    case 9:
     case 11: return 30;
-    default : return 31;
+    default: return 31;
   }
 }
 
+function isLastDayOfYear(date, month, year) {
+  const isLastDay = date === isLastDayOfMonth(month, year);
+
+  return isLastDay && month === 12;
+}
+
+function isInvalidDate(date, month, year) {
+  if (date === 31 && month === 12 && year === 9999) {
+    return true;
+  }
+
+  const maxDaysInMonth = isLastDayOfMonth(month, year);
+
+  return date === 0 || date > maxDaysInMonth || month > 12 || month === 0;
+}
+
 function incrementDate(date, month, year) {
-  if (date === isLastDayOfMonth(month, year) && month === 12) {
+  if (isInvalidDate(date, month, year)) {
+    return "Invalid Date";
+  }
+
+  if (isLastDayOfYear(date, month, year)) {
     date = 1;
     month = 1;
     year += 1;
@@ -96,9 +116,13 @@ function testAll() {
   testIncrementDate("31 October", "31-10-2025", "01-11-2025");
   testIncrementDate("30 April", "30-04-2025", "01-05-2025");
   testIncrementDate("30 June", "30-06-2025", "01-07-2025");
-  testIncrementDate("28 February (Leap year)", "28-02-2024", "29-02-2024");
-  testIncrementDate("28 February (Non leap year)", "28-02-2025", "01-03-2025");
+  testIncrementDate("28 Feb (Leap year)", "28-02-2024", "29-02-2024");
+  testIncrementDate("28 Feb (Non leap year)", "28-02-2025", "01-03-2025");
   testIncrementDate("31 December", "31-12-2024", "01-01-2025");
+  testIncrementDate("More than month in year", "12-13-2024", "Invalid Date");
+  testIncrementDate("0 Jan", "00-01-2024", "Invalid Date");
+  testIncrementDate("first date", "01-01-0000", "02-01-0000");
+  testIncrementDate("Last date", "31-12-9999", "Invalid Date");
 }
 
 function main() {
