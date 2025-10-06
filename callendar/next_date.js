a9941286ad0d0fd6fcbf665878773daeaa5faee2
Dinesh Kumar <mk9211620@gmail.com>
2025-10-06 12:13:29 +0530

    Writing simple passing test case and prepared test framework for passing tests

--- callendar/next_date.js
+++ callendar/next_date.js
@@ -1,5 +1,26 @@
+function incrementDate(date, month, year) {
+  return "02-01-2025";
+}
+
+function composeMessage(mark, message) {
+  console.log(mark, message);
+}
+
+function testIncrementDate(message, date, month, year, expected) {
+  const actual = incrementDate(date, month, year);
+  const mark = actual === expected ? '✅' : '❌';
+
+  if (actual === expected) {
+    composeMessage(mark, message);
+  }
+}
+
+function testAll() {
+  testIncrementDate("First day of year", 1, 1, 2025, "02-01-2025");
+}
+
 function main() {
-  console.log("Still Working");
+  testAll();
 }
 
 main();
