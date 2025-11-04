75da4d7883fc77d44c31f7e701af1c8ea3db94ab
Dinesh Kumar <mk9211620@gmail.com>
2025-11-04 21:18:27 +0530

    Fixed hygiene of code.

--- ap/ap.js
+++ ap/ap.js
@@ -6,10 +6,11 @@ let limit = 7;
 let numberOfTerms = 1;
 let currentTerm = firstTerm;
 
-console.log("First Term:", firstTerm, "Difference:", difference, "Limit:", limit)
+console.log("First Term:", firstTerm, "Difference:", difference, "Limit:", limit);
 console.log(firstTerm);
-while(numberOfTerms < limit){
-    currentTerm += difference;
-    console.log(currentTerm);    
-    numberOfTerms++;
+
+while (numberOfTerms < limit) {
+  currentTerm += difference;
+  console.log(currentTerm);
+  numberOfTerms++;
 }
